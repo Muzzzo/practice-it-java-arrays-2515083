35cd1f8287f363c3d60fe0e0695eea55c907a4f5
Muzzo <51519604+Muzzzo@users.noreply.github.com>
2024-05-09 12:30:31 +0000

    arrays in java-linkedin-practice

--- src/App.java
+++ src/App.java
@@ -1,6 +1,10 @@
 public class App {
   
   public static void main(String[] args) {
+    double[] lotteryNums = {45,92,38,33,21};//new double[];
+    System.out.println(lotteryNums[2]);
+    lotteryNums[2]=29;
+    System.out.println(lotteryNums[2]);
     
   }
 }
